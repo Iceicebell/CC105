7b8dc185fd9ca0b4f2e9086222a4f02dafa85f39
ABuenaflor <112681058+ABuenaflor@users.noreply.github.com>
2023-09-05 17:05:51 +0800

    1st add

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@ $database="pharmacy_management";
 $connect=new mysqli($host,$username,$password,$database);
 
 if($connect->connect_error){
-    die('Connection failed'.$connection->connect_error);
+    die('Connection failed'.$connect->connect_error);
 }
 else{
     return $connect;
